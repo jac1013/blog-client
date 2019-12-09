64af8b9fc33684a99b2bfb3410358c322d830c7b
Miguelangelito <miguel_gantz89@hotmail.com>
2019-12-09 18:03:27 -0500

    Fix test error connected with link

--- src/index.js
+++ src/index.js
@@ -11,15 +11,11 @@ require('dotenv').config();
 
 axios.defaults.baseURL = 'URL';
 
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default app;
