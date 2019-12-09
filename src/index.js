db74e47f1c65a9915ea459ce8415697c044d9bcf
Miguelangelito <miguel_gantz89@hotmail.com>
2019-12-09 17:50:20 -0500

    Add incomplete header

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import {BrowserRouter} from 'react-router-dom';
 
 require('dotenv').config();
 
 axios.defaults.baseURL = 'URL';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
