b2a33cb25a8eb729e04179cb52205f764121868d
Miguelangelito <miguel_gantz89@hotmail.com>
2019-12-12 14:59:45 -0500

    Remove App.js comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import './App.css';
-// import Button from 'react-bootstrap/Button';
-// import {Animated} from "react-animated-css";
 import Header from "./components/Navigation/Header/Header";
 
 function App() {
@@ -11,12 +9,6 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-      {/*<Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>*/}
-      {/*  <div className="App">*/}
-      {/*    <h1>React App</h1>*/}
-      {/*    <Button variant="danger">Hello</Button>*/}
-      {/*  </div>*/}
-      {/*</Animated>*/}
     </React.Fragment>
   );
 }
