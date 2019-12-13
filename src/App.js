e569cd5cb4d3b18507313ff7d537bd5556dfe5fb
Miguelangelito <miguel_gantz89@hotmail.com>
2019-12-13 04:32:34 -0500

    Add WrittenBy component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from "./components/Navigation/Header/Header";
 import ArticleBody from "./components/ArticleBody/ArticleBody";
 import Title from "./components/Title/Title";
+import WrittenBy from "./components/WrittenBy/WrittenBy";
 
 function App(props) {
   console.log(process.env.REACT_APP_API_URL);
@@ -12,6 +13,7 @@ function App(props) {
     <React.Fragment>
       <Header />
       <Title fontSize="100px">Hello</Title>
+      <WrittenBy link="https://www.youtube.com/watch?v=uuBETyA_yxc" author="@Joseph" date="January 14, 2017"/>
       <ArticleBody />
     </React.Fragment>
   );
