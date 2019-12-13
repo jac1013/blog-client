24e181625914df59729fa63dbd0a98a6e4c14ffd
Miguelangelito <miguel_gantz89@hotmail.com>
2019-12-13 04:07:26 -0500

    Add title component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Navigation/Header/Header";
 import ArticleBody from "./components/ArticleBody/ArticleBody";
+import Title from "./components/Title/Title";
 
-function App() {
+function App(props) {
   console.log(process.env.REACT_APP_API_URL);
 
   return (
     <React.Fragment>
       <Header />
+      <Title fontSize="100px">Hello</Title>
       <ArticleBody />
     </React.Fragment>
   );
