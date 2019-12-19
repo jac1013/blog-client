8cda858778f9c73e5618073ec5cdf55af27a8f94
Carlos Cardenas <57842910+iCarloscz@users.noreply.github.com>
2019-12-18 22:01:15 -0400

    Footer comp rev1 (#15)
    * Footer component fixed
    
    * Footer component fixed
    
    * Footer component totally fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 import './App.css';
 import Header from "./components/Navigation/Header/Header";
+import ArticleBody from "./components/ArticleBody/ArticleBody";
+import Footer from "./components/Footer-Comp/Footer.js";
 import MainBody from "./components/MainBody/MainBody";
+
 
 function App(props) {
   console.log(process.env.REACT_APP_API_URL);
@@ -10,7 +13,9 @@ function App(props) {
   return (
     <React.Fragment>
       <Header />
+      <ArticleBody />
       <MainBody />
+      <Footer />
     </React.Fragment>
   );
 }
