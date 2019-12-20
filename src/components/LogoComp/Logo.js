6a5ee8f596484dee07ddf4dd51d1b79a4d0234cf
iCarloscz <carlos.cardenasg1@gmail.com>
2019-12-19 22:29:34 -0430

    Logo Component

--- src/components/LogoComp/Logo.js
+++ src/components/LogoComp/Logo.js
@@ -1,12 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import LogoImg from "../../assets/LogoImg.jpg";
-import "../LogoComp/Logo.module.css";
+import LogoImg from "../../assets/Logo1.png";
 import classes from "../LogoComp/Logo.module.css";
 
 const Logo = (props) => (
     <div className={classes.Logo}>
-        <Link to={props.link}>
+        <Link to="/">
             <img src={LogoImg} alt="Logo" />
         </Link>
     </div>
