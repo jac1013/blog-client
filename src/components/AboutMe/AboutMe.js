e1f429530780278a0236936c4ed28cb568a3b551
iCarloscz <carlos.cardenasg1@gmail.com>
2020-01-06 13:40:15 -0430

    AboutMe Component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aboutme from "../../containers/AboutMe/AboutMe.js";
+import Title from "../../components/Title/Title";
+import classes from "../AboutMe/AboutMe.module.css";
+import PictureRef from "../../assets/Picref.jpg";
+const AboutMe = () => (
+  <div className={classes.About}>
+    <Title fontSize="60px">About Joseph Arrieta.</Title>
+    <div>
+      <img img src={PictureRef} alt="Profile" />
+      <Aboutme />
+    </div>
+  </div>
+);
+
+export default AboutMe;
