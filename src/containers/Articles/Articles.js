057c55a56035b6f3b6c68d835b9ac3505b7ddaf0
Miguelangelito <miguel_gantz89@hotmail.com>
2020-01-18 16:36:50 -0500

    Fix code smells

--- src/containers/Articles/Articles.js
+++ src/containers/Articles/Articles.js
@@ -5,38 +5,38 @@ import Title from "../../components/Title/Title";
 import classes from "./Articles.module.css";
 
 const Articles = () => {
-  const [posts, setPosts] = useState([{
+  const [posts] = useState([{
       id: '1',
       title: 'asdspgada',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      body: 'Quisque nec purus est. Vivamus non lorem nunc. Curabitur semper, nibh eu mollis finibus, metus eros dapibus orci, ullamcorper suscipit dolor ligula sed justo.',
       date: 'asdsad',
       link: '1'
     },
     {
       id: '2',
       title: 'asdsada',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam semper gravida ligula. Maecenas aliquet urna eros, ac varius nunc pretium a.',
       date: 'asdsad',
       link: '1'
     },
     {
       id: '3',
       title: 'asdsada',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      body: 'Curabitur fermentum consequat pretium. Curabitur placerat non erat gravida feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac.',
       date: 'asdsad',
       link: '1'
     },
     {
       id: '4',
       title: 'asdsada',
-      body: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      body: 'Mauris pellentesque malesuada justo. In eu felis nec nisi semper ultricies quis vitae ligula. Morbi sit amet mauris auctor, fermentum arcu in, varius nisl.',
       date: 'asdsad',
       link: '1'
     },
     {
       id: '5',
       title: 'asdsada',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      body: 'Praesent convallis, erat ut elementum vulputate, enim elit tincidunt mi, vitae mattis tortor augue et magna. Ut ut iaculis sapien. Duis sed accumsan augue.',
       date: 'asdsad',
       link: '1'
     }]
@@ -50,9 +50,9 @@ const Articles = () => {
         {posts.map(post => (
           <ArticlesItem
             title={post.title}
-            body={post.body.slice(0, 200) + '...'}
+            body={`${post.body.slice(0, 150)}...`}
             date={post.date}
-            link={"/article/" + post.link}
+            link={`/article/${post.link}`}
             key={post.id} />
         ))}
       </div>
