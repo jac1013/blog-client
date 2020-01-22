20f2f49616366800b77c47209e77930ddfe88b54
iCarloscz <carlos.cardenasg1@gmail.com>
2020-01-22 09:26:20 -0430

    fixed problem with workflow

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import AboutMeContent from "./AboutMe.js";
+import AboutMeContent from "../../components/AboutMeContent/AboutMeContent.js";
 import Title from "../../components/Title/Title";
 import classes from "./AboutMe.module.css";
 import PictureRef from "../../assets/Picref.jpg";
+
+
 const AboutMe = () => (
   <div className={classes.About}>
     <Title fontSize="60px" title='About Joseph Arrieta.'></Title>
