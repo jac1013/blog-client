e54afe20946bb354bef62d5a41e6b6a0627c3892
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-02-28 10:29:57 -0430

    uncompleted About Me component

--- src/components/Footer-Comp/Footer.js
+++ src/components/Footer-Comp/Footer.js
@@ -2,24 +2,32 @@ import React, { useState } from "react";
 import classes from "../Footer-Comp/Footer.module.css";
 
 function Footer() {
-    const [blogName] = useState(["CodeMoulding.blog"])
-    const [copyrightYear] = useState(["Copyright ©2019"])
-    const [author] = useState (["Joseph Arrieta."])
-    const [githubUrl] = useState (["http://github.com/jac1013"])
-    const [linkedinUrl] = useState (["http://linkedin.com/in/joseph-arrieta-81b8887b/w"])
-    
+    const [blogFooter] = useState([{
+        id: "1",
+        name: "codemoulding.blog",
+        author: "Joseph Arrieta",
+        githuburl: "https://github.com/jac1013",
+        linkedinurl: "http://linkedin.com/in/joseph-arrieta-81b8887b/w",
+        copyright: "Copyright ©2019"
+
+    }]
+    );
+
 return(
-    <footer className={classes.Footer}>
-        <div className={classes.Blog}>
-            <p>{blogName}</p>
-        </div>
-        <div>
-            <p>Keep in touch via <a className={classes.KeepinTouch} href={githubUrl}>Github</a> or <a className={classes.KeepinTouch} href={linkedinUrl}>LinkedIn</a></p>
-        </div>
-        <div><p>{copyrightYear} {author} All rights reserved.</p></div>
+    <footer>
+        {blogFooter.map(blogFooter =>(
+                <div key={blogFooter.id} className={classes.Footer}>
+                    <div className={classes.Blog}>{blogFooter.name}</div>
+                    <div className={classes.Bugged}>
+                        <p>Keep in touch via <a className={classes.Contact} href={blogFooter.githuburl}>GitHub</a> or <a className={classes.Contact} href={blogFooter.linkedinurl}>LinkedIn</a></p>
+                    </div>
+                    <div>
+                        <p>{blogFooter.copyright} {blogFooter.author} All right reserved.</p>
+                    </div>
+                    <div><p>HappyCoding!</p></div>
+                </div>
+            ))}
     </footer>
     );
-}
-
-
+};
 export default Footer;
--- src/containers/AboutMeContainer/AboutMe.js
+++ src/containers/AboutMeContainer/AboutMe.js
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import AboutMeContent from "../../components/AboutMeComponent/AboutMeContent.js";
 import Title from "../../components/Title/Title";
 import classes from "../AboutMeContainer/AboutMe.module.css";
-import InfoAboutMe from '../AboutMeContainer/InfoAboutMe.json'
 
+function AboutMe() {
+  const [aboutData, setAboutData] = useState({});
+  
+  const fetchData = async () => {
+    const response = await axios.get('https://api.codecarver.dev/article/3');
+    setAboutData(response.data.result);
+    console.log(response);
+  };
 
-const AboutMe = () => {
-  const [aboutMeInfo] = useState(InfoAboutMe)
+    useEffect(()=> {
+      fetchData();
+    },[]);
 
-    let aboutMe = null;
-
-  aboutMe = (
+return(
     <div className={classes.About}>
-      {aboutMeInfo.map(aboutMeInfo => (
-        <div key={aboutMeInfo.id}>
-          <div>
-            <Title fontSize="60px" title={aboutMeInfo.title}></Title>
-          </div>
-            <img src={aboutMeInfo.link} alt="Profile" />
-            <AboutMeContent source={aboutMeInfo.content} />
+        <div key={aboutData.id}>
+          <Title fontSize="60px" title={aboutData.title} />
+          <AboutMeContent source={aboutData.body} />
         </div>
-      ))}
     </div>
   );
-  return aboutMe
-};
+  };
 
 export default AboutMe;
