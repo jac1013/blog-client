19841be6cfa62b0d399c60ce9e43f389eb6fadbf
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-03-02 14:55:54 -0430

    code smell fixed

--- src/components/Footer-Comp/Footer.js
+++ src/components/Footer-Comp/Footer.js
@@ -20,7 +20,9 @@ function Footer() {
                         <p>{footerData.name}</p>
                     </div>
                     <div>
-                        <p>Keep in Touch via <a className={classes.KeepInTouch} href={footerData.githuburl}>GitHub</a> or <a className={classes.KeepInTouch} href={footerData.linkedinurl}>LinkedIn</a></p>
+                        <p>Keep in Touch via <a className={classes.KeepInTouch} href={footerData.githuburl}>GitHub</a> or 
+                            <a className={classes.KeepInTouch} href={footerData.linkedinurl}>LinkedIn</a>
+                        </p>
                     </div>
                     <div>
                         <p>{footerData.copyright} {footerData.author}</p>
@@ -33,29 +35,5 @@ function Footer() {
         </footer>
     );
     return footerInfo;
-};
-export default Footer;
-/*function Footer() {
-    const [blogName] = useState(["CodeMoulding.blog"])
-    const [copyrightYear] = useState(["Copyright ©2019"])
-    const [author] = useState (["Joseph Arrieta."])
-    const [githubUrl] = useState (["http://github.com/jac1013"])
-    const [linkedinUrl] = useState (["http://linkedin.com/in/joseph-arrieta-81b8887b/w"])
-
-    return(
-    <footer className={classes.Footer}>
-        <div className={classes.Blog}>
-            <p>{blogName}</p>
-        </div>
-        <div>
-            <p>Keep in touch via <a className={classes.Contact} href={githubUrl}>Github</a> or <a className={classes.Contact} href={linkedinUrl}>LinkedIn</a></p>
-        </div>
-        <div>
-            <p>{copyrightYear} {author} All rights reserved.</p>
-        </div>
-        <div><p>Happy Coding!</p></div>
-    </footer>
-    );
 }
-
-  export default Footer;*/
+export default Footer;
--- src/containers/AboutMeContainer/AboutMe.js
+++ src/containers/AboutMeContainer/AboutMe.js
@@ -6,7 +6,6 @@ import classes from "../AboutMeContainer/AboutMe.module.css";
 
 function AboutMe() {
   const [aboutData, setAboutData] = useState({});
-  
   const fetchData = async () => {
     const response = await axios.get('https://api.codecarver.dev/article/3');
     setAboutData(response.data.result);
@@ -25,6 +24,5 @@ return(
         </div>
     </div>
   );
-  };
-
+  }
 export default AboutMe;
