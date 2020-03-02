aba8d80de9a11a9b8f1da51a73c672da71e27cc5
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-03-02 14:34:42 -0430

    Footer Component useState hook

--- src/components/Footer-Comp/Footer.js
+++ src/components/Footer-Comp/Footer.js
@@ -2,6 +2,40 @@ import React, { useState } from "react";
 import classes from "../Footer-Comp/Footer.module.css";
 
 function Footer() {
+    const [footerData] = useState([{
+        "id": "1",
+        "name": "codecarver.dev",
+        "githuburl": "https://githu.com/jac1013",
+        "linkedinurl": "https://linkedin.com/in/joseph-arrieta-81b8887b/w",
+        "copyright": "Copyright ©2019.",
+        "author": "Joseph Arrieta."
+
+    }]);
+    let footerInfo = null;
+    footerInfo = (
+        <footer>
+            {footerData.map(footerData =>(
+                <footer key={footerData.id} className={classes.Footer}>
+                    <div className={classes.Webname}>
+                        <p>{footerData.name}</p>
+                    </div>
+                    <div>
+                        <p>Keep in Touch via <a className={classes.KeepInTouch} href={footerData.githuburl}>GitHub</a> or <a className={classes.KeepInTouch} href={footerData.linkedinurl}>LinkedIn</a></p>
+                    </div>
+                    <div>
+                        <p>{footerData.copyright} {footerData.author}</p>
+                    </div>
+                    <div>
+                        <p>Happy Coding!</p>
+                    </div>
+                </footer>
+            ))}
+        </footer>
+    );
+    return footerInfo;
+};
+export default Footer;
+/*function Footer() {
     const [blogName] = useState(["CodeMoulding.blog"])
     const [copyrightYear] = useState(["Copyright ©2019"])
     const [author] = useState (["Joseph Arrieta."])
@@ -24,4 +58,4 @@ function Footer() {
     );
 }
 
-  export default Footer;
+  export default Footer;*/
