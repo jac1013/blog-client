24d2181389df41fa55fce852addf96ac4c1189bd
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-04-07 07:15:55 -0430

    Footer Component recieve it data from State

--- src/components/Footer-Comp/Footer.js
+++ src/components/Footer-Comp/Footer.js
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import classes from "../Footer-Comp/Footer.module.css";
 
 function Footer() {
-    const [blogName] = useState(["CodeMoulding.blog"])
-    const [copyrightYear] = useState(["Copyright ©2019"])
-    const [author] = useState (["Joseph Arrieta."])
-    const [githubUrl] = useState (["http://github.com/jac1013"])
-    const [linkedinUrl] = useState (["http://linkedin.com/in/joseph-arrieta-81b8887b/w"])
+    const [blogData] = useState({
+        blogName:"Codecarver.dev",
+        copyRight: "Copyright ©2019",
+        autor:"Joseph Arrieta",
+        githubUrl:"http://github.com/jac1013",
+        linkedinUrl:"http://linkedin.com/in/joseph-arrieta-81b8887b/w"})
     return(
     <footer className={classes.Footer}>
         <div className={classes.Blog}>
-            <p>{blogName}</p>
+            <p>{blogData.blogName}</p>
         </div>
         <div>
-            <p>Keep in touch via <a className={classes.Contact} href={githubUrl}>Github</a> or <a className={classes.Contact} href={linkedinUrl}>LinkedIn</a></p>
+            <p>Keep in touch via <a className={classes.Contact} href={blogData.githubUrl}>Github </a>
+            or<a className={classes.Contact} href={blogData.linkedinUrl}> LinkedIn</a></p>
         </div>
         <div>
-            <p>{copyrightYear} {author} All rights reserved.</p>
+            <p>{blogData.copyRight} {blogData.author} All rights reserved.</p>
         </div>
         <div><p>Happy Coding!</p></div>
     </footer>
