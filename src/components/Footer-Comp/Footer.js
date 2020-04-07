61aadc7ed18e9ce5efca6ffa19cfae285a266dd6
Carlos Cardenas <57842910+iCarloscz@users.noreply.github.com>
2020-04-07 08:58:45 -0400

    Update Footer.js

--- src/components/Footer-Comp/Footer.js
+++ src/components/Footer-Comp/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
         autor:"Joseph Arrieta",
         githubUrl:"http://github.com/jac1013",
         linkedinUrl:"http://linkedin.com/in/joseph-arrieta-81b8887b/w"})
-    
     return(
     <footer className={classes.Footer}>
         <div className={classes.Blog}>
