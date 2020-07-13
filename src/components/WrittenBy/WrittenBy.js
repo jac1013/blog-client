cd4d1356c5fab31fdf616d673420890a5b607925
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-07-13 19:22:41 -0430

    uninstalled unnecessary package from the project

--- src/components/WrittenBy/WrittenBy.js
+++ src/components/WrittenBy/WrittenBy.js
@@ -4,7 +4,7 @@ import classes from './WrittenBy.module.css';
 const WrittenBy = (props) => {
 
   return (
-    <p className={classes.WrittenBy}>Written by <a href={props.link} target="_blank" rel="noopener noreferrer" ></a> on {props.date}</p>
+    <p className={classes.WrittenBy}>Written by <a href={props.link} target="_blank" rel="noopener noreferrer" >{props.author}</a> on {props.date}</p>
   );
 };
 
