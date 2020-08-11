754691149ac05ce97a65b1c2f0e75a10c2b9600b
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-08-11 16:52:29 -0400

    Transition Styles added to the blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import './App.css';
 import Header from "./components/Navigation/Header/Header";
 import Footer from "./components/Footer-Comp/Footer.js";
@@ -13,18 +13,35 @@ import NotFound from './containers/PageNotFound/NotFound.js';
 function App(props) {
 
   return (
-    <React.Fragment>
-      <Header />
-      <Switch>
-        <Route path="/article/:id" component={MainBody} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about-me" component={AboutMe} />
-        <Route path="/" exact component={Home} />
+    <Router>
+      <div>
+        <Header />
+        <Route
+          render={({ location }) => (
+            <TransitionGroup className="container">
+              <CSSTransition
+                appear={true}
+                key={location.key}
+                timeout={{ enter: 400, exit: 200 }}
+                classNames="fade"
+              >
+          <div className="inner">
+          <Switch>
+            <Route path="/article/:id" component={MainBody} />
+            <Route path="/articles" component={Articles} />
+            <Route path="/about-me" component={AboutMe} />
+            <Route path="/" exact component={Home} />
 
-        <Route path="" component={NotFound} />
-      </Switch>
+            <Route path="" component={NotFound} />
+          </Switch>
+          </div>
+          </CSSTransition>
+          </TransitionGroup>
+          )}
+          />
+      </div>
       <Footer />
-    </React.Fragment>
+  </Router>
   );
 }
 
