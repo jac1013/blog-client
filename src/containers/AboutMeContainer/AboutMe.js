52334610d8e32dd2e6b50f6fd9a8a745374a523f
Carlos Cardenas <carlos.cardenasg1@gmail.com>
2020-08-20 10:34:50 -0400

    Loader now appear on AboutMe view

--- src/containers/AboutMeContainer/AboutMe.js
+++ src/containers/AboutMeContainer/AboutMe.js
@@ -4,34 +4,41 @@ import Title from "../../components/Title/Title";
 import classes from "../AboutMeContainer/AboutMe.module.css";
 import avatar from "../../assets/Picref.jpg";
 import MarkdownRenderer from '../../components/MarkdownRenderer/MarkdownRenderer';
+import Loader from "../../components/LoaderComponent/Loader.js";
 
 const dataUrl = "https://api.codecarver.dev/article/8";
 const AboutMe = () => {
   const [aboutMeData, setAboutMeData] = useState([])
-  const fetchData = async () => {
-    const response = await axios.get(dataUrl);
-    setAboutMeData(response.data.result)
-  }
-    useEffect(()=> {
-      fetchData();
-    },[])
-
-
-    let aboutMe = null;
-    
-  aboutMe = (
-    <div className={classes.About}>
-        <div key={aboutMeData.id}>
-          <div>
-            <Title fontSize="60px" title={aboutMeData.title}></Title>
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(()=> {
+    const fetchData = async ()=> {
+      setIsLoading(true)
+      const response = await axios.get(dataUrl)
+      setAboutMeData(response.data.result)
+      setIsLoading(false)
+    }
+    fetchData();
+  }, [])
+  return(
+    <React.Fragment>
+      {isLoading ? (
+        <div className={classes.Loader}>
+          <Loader />
+        </div>):(
+          <div className={classes.About}>
+            <div key={aboutMeData.id}>
+              <div>
+                <Title fontSize="60px" title={aboutMeData.title}></Title>
+              </div>
+              <div className={classes.Content} >
+                <img className={classes.Avatar} src={avatar} alt="avatar" /> 
+                <MarkdownRenderer source={aboutMeData.body} />
+              </div>
+            </div>
           </div>
-          <div className={classes.Content} >
-            <img className={classes.Avatar} src={avatar} alt="avatar" /> 
-            <MarkdownRenderer source={aboutMeData.body} />
-          </div>
-        </div>
-    </div>
-  );
-  return aboutMe
+      )}
+    </React.Fragment>
+  )
 };
 export default AboutMe;
