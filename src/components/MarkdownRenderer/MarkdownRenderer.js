b312a7938c18326134fff9b34dee32f6e78f792e
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-08-27 17:05:06 -0500

    Add a markdown renderer with more options

--- src/components/MarkdownRenderer/MarkdownRenderer.js
+++ src/components/MarkdownRenderer/MarkdownRenderer.js
@@ -1,9 +1,56 @@
 import React from 'react';
-import marked from 'marked';
 import sanitize from 'sanitize-html'
+import twemoji from 'twemoji';
+import hljs from 'highlight.js'
+import "highlight.js/styles/dracula.css"
+
+hljs.initHighlightingOnLoad()
+
+var md = require('markdown-it')({
+  html:         true,        // Enable HTML tags in source
+  xhtmlOut:     false,        // Use '/' to close single tags (<br />).
+                              // This is only for full CommonMark compatibility.
+  breaks:       false,        // Convert '\n' in paragraphs into <br>
+  langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
+                              // useful for external highlighters.
+  linkify:      true,        // Autoconvert URL-like text to links
+
+  // Enable some language-neutral replacement + quotes beautification
+  typographer:  true,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  //
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+  quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externally.
+  // If result starts with <pre... internal wrapper is skipped.
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+}).use(require('markdown-it-deflist'))
+  .use(require('markdown-it-emoji'))
+  .use(require('markdown-it-footnote'))
+  .use(require('markdown-it-sub'))
+  .use(require('markdown-it-sup'));
+
+md.renderer.rules.emoji = function(token, idx) {
+  return twemoji.parse(token[idx].content);
+};
 
 const MarkdownRenderer = props => {
-  const toHtml = marked(props.source || "");
+  const toHtml = md.render(props.source || "");
   const sanitized = sanitize(toHtml, {allowedTags: false, allowedAttributes: false})
   return <div dangerouslySetInnerHTML={{__html:sanitized}} />
 };
