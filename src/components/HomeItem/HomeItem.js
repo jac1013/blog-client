5bfd1b2a4a905fa7555310f031cfb5bbe5a5c91c
Joseph Arrieta <jac.wshmstr@gmail.com>
2020-08-27 17:14:03 -0500

    Add markdown to the body preview of home articles

--- src/components/HomeItem/HomeItem.js
+++ src/components/HomeItem/HomeItem.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 
 import Title from "../Title/Title";
 import classes from "./HomeItem.module.css";
+import MarkdownRenderer from '../MarkdownRenderer/MarkdownRenderer';
 
 const HomeItem = (props) => (
   <div className={classes.Article}>
     <Title fontSize="1.5em" title={props.title}></Title>
-    <p>{props.body}</p>
+    <MarkdownRenderer source={props.body} />
     <Link to={props.link} className={classes.KeepReading}>Keep Reading →</Link>
   </div>
 );
